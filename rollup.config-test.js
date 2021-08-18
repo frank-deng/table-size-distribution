ce137063ac72d12f37d68b976fe4dd6aa9c0c2db
frank-deng <13382084679@163.com>
2021-08-18 22:10:38 +0800

    test build removed babel

--- rollup.config-test.js
+++ rollup.config-test.js
@@ -1,14 +1,8 @@
-import babel from 'rollup-plugin-babel';
 export default{
     input: 'src/test.js',
     output:{
         file:"__test__.js",
         format:'commonjs',
         sourcemap:false
-    },
-    plugins: [
-        babel({
-            exclude: 'node_modules/**',
-        })
-    ]
+    }
 };
